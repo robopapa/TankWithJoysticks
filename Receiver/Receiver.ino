a72c07bdfdb8da4aeb004614ab0e5c584d176f78
nirziv <ziv.nir77@gmail.com>
2020-05-21 00:14:51 -0700

    Updated Tank Code
    Updated movement, lift and button press. Not tested yet.

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,55 +1,79 @@
+/**************************************************************************************************************/
+/******** This code can be used with anything you need. However, it was written for this Tank in mind *********/
+/******** https://www.sparkfun.com/products/retired/12092                                             *********/
+/**************************************************************************************************************/
+
 #include <Wire.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <printf.h>
 #include <Servo.h>
-
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h" 
 
-/*-----( Declare Constants and Pin Numbers )-----*/
-#define CE_PIN   5
-#define CSN_PIN 6
-#define LEFT_JOYSTICK 0
-#define RIGHT_JOYSTICK 1
+/************************************************/
+/****** Declare Constants and Pin Numbers *******/
+/************************************************/
+#define CE_PIN          5
+#define CSN_PIN         6
+#define LEFT_JOYSTICK   0
+#define RIGHT_JOYSTICK  1
+#define SERVO_PIN       9
+#define BREAKE          0
+/*********** Adafruit Motor Shield V2.3 ************/
 
+Adafruit_MotorShield AFMS     = Adafruit_MotorShield();
+Adafruit_DCMotor *leftMotor   = AFMS.getMotor(3);
+Adafruit_DCMotor *rightMotor  = AFMS.getMotor(4);
+
+/***************************************************/
+
+/******************* Servo setup *******************/
+
+Servo liftupServo;
+
+/**************************************************/
+/******************* Radio setup ******************/
 
 const byte pipe[][6] = {"00006"}; // Define the transmit pipe
+RF24 radio(CE_PIN, CSN_PIN);      // Create a Radio
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
-Servo myservo;
+/**************************************************/
 
-RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+/*************** Transmitted Values ***************/
 
 typedef struct {
   int vPosition;
   int hPosition;
   bool button;
 } Joystick;
+Joysticks joystick[2];
 
-Joystick joystick[2];
+/**************************************************/
+
+/**************** Global Variables ****************/
+const bool Pressed       = true;
+const bool Released      = false;
+int leftMotorDirection   = FORWARD;
+int rightMotorDirection  = FORWARD;
+bool isLiftUp            = false;
+bool buttonStatus        = false;
+/**************************************************/
+
 void setup()   
 {
   Serial.begin(9600);
   delay(1000);
   Serial.println("Nrf24L01 Receiver Starting");
   radio.begin();
-  //radio.setPALevel(RF24_PA_LOW);
-  //radio.setDataRate(RF24_2MBPS);
-  //radio.setChannel(124);
   radio.openReadingPipe(0,pipe[0]);
   radio.startListening();
-  //radio.printDetails();
-
-  myservo.attach(9); // can also use 8
+  
+  liftupServo.attach(SERVO_PIN); // can also use 8
   AFMS.begin();
 }
 
-bool leftMotorForward = true;
-bool rightMotorForward = true;
 void loop()
 {
   
@@ -60,14 +84,27 @@ void loop()
     int val[2];
     while (!done)
     {
-      // Fetch the data payload
-//      radio.read( &joysticks[0], sizeof(joysticks) );
-//      radio.read( &joysticks[1], sizeof(joysticks) );
-//      Serial.print("sizeof(joystick) = ");
-//      Serial.println(sizeof(Joystick));
-      radio.read(&joystick,sizeof(Joystick)*2);
-      // PrintValues();
+      radio.read(&joysticks,sizeof(Joystick)*2);
+      // now we have the data that was transmitted, so we need to do some logic.
+      if(joysticks[RIGHT_JOYSTICK].button == 1 && buttonStatus == Released) {
+          buttonStatus  = Pressed;
+          isLiftUp          = true;
+      } else if (joysticks[RIGHT_JOYSTICK].button == 0 && buttonStatus == Pressed) {
+        buttonStatus = Released;
+      }
+
+      if(isLiftup) {
+        ProcessLiftUp();
+      } else {
+        ProcessMovement();
+      }
+
+
+#ifdef __DEBUG__
+      PrintValues();
+#endif
       delay(250);
+      done = radio.available();
     }
   }
   else
@@ -76,7 +113,46 @@ void loop()
       delay(250);
   }
   
-}//--(end main loop )---
+}
+
+void ProcessMovement() {
+  // Left Motor
+  int value = joysticks[LEFT_JOYSTICK].vPosition;
+  leftMotorDirection = GetDirection(value);
+  int speed = GetSpeed(value, leftMotorDirectionFORWARD);
+  leftMotor->setSpeed(speed);
+  leftMotor->run(leftMotorDirection);
+
+  // Right Motor
+  value = joysticks[RIGHT_JOYSTICK].vPosition;
+  rightMotorDirection = GetDirection(value);
+  speed = GetSpeed(value, rightMotorDirection);
+  rightMotor->setSpeed(speed);
+  rightMotor->run(leftMotorDirection);
+}
+
+int GetDirection(int value) {
+  return value < 512 ? BACKWARD : FORWARD;
+}
+
+int GetSpeed(int value, int direction) {
+  return 
+        direction == FORWARD ? 
+        map ( value, 512, 1024, 0, 255 ) : 
+        map ( value, 0, 512, 255, 0 );
+}
+
+void ProcessLiftUp(){
+  // stop movement 
+  leftMotor->setSpeed(BREAKE);
+  rightMotor->setSpeed(BREAKE);
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+
+  int value = joysticks[RIGHT_JOYSTICK].vPosition;
+  int angle = map(value, 512, 1024, 0, 180);
+  liftupServo.write(angle);
+}
 
 void PrintValues() {
   Serial.print("Left Joystick:\t");
@@ -92,8 +168,3 @@ void PrintValues() {
   Serial.print("\t");
   Serial.println(joystick[RIGHT_JOYSTICK].button);
 }
-
-/*-----( Declare User-written Functions )-----*/
-
-//NONE
-//*********( THE END )***********
